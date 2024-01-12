72a74569961de83b7d037276ebf2604cbc715cde
adamishakad <adamishag1@gmail.com>
2024-01-11 22:57:35 -0800

    Updated Program File

--- Program.cs
+++ Program.cs
@@ -7,6 +7,17 @@
             Console.WriteLine("Hello, Students!");
             Console.WriteLine("GRU");
             Console.WriteLine("NO, no, no.");
+            Console.WriteLine("GRU");
+            Console.WriteLine("OK, my turn");
+            Console.WriteLine("GRU" + " calmly reaches into his coat and pulls out a small gun that transfc");
+            Console.WriteLine("He said \"What???\"\n");
+            Console.WriteLine("And I said \"I know!\"");
+            Console.WriteLine("AGNESUpper");
+            Console.WriteLine("\"Oh my gosh look at that fluffy unicorn!\"\n");
+            Console.WriteLine("A FLUFFY UNICORN");
+            Console.WriteLine("is the main prize at the shooting gallery.\n");
+            Console.WriteLine("AGNESUpper");
+            Console.WriteLine("\"He's so fluffy I'm gonna die!!!\n");
 
         }
     }
